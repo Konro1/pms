c2476b0e6059833847f3dd8c18bcb98a7bd22692
Yuriy Myrosh <yuriy.myrosh@lemberg.co.uk>
2014-09-15 19:12:03 +0300

    db model add

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -132,6 +132,12 @@ Kohana::modules(array(
 	));
 
 /**
+ * Autoload composer libraries
+ *
+ */
+require APPPATH . 'modules/autoload.php';
+
+/**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
  */
